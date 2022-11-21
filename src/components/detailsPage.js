f5bfd853877d8c50f386eeb6bb9166c6542f6da0
kaizipaul <kaizipaul@gmail.com>
2022-11-21 15:14:31 +0300

    Added boilerplate files for the project

---
+++ src/components/detailsPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const detailsPage = () => (
+  <h2>Details</h2>
+);
+
+export default detailsPage;
---
+++ src/components/homePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const homePage = () => (
+  <h2>Home</h2>
+);
+
+export default homePage;

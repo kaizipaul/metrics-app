015d8371026b56cc796931468e8f464f3c78cd06
Paul Kaizirege <kaizipaul@gmail.com>
2024-02-28 18:33:24 +0300

    Add ChakraUI component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import HomePage from './components/homePage';
 import DetailsPage from './components/detailsPage';
 import NavBar from './components/navBar';
+import Search from './components/search';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route path="/details/:id" element={<DetailsPage />} />
-      </Routes>
+      <ChakraProvider>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/details/:id" element={<DetailsPage />} />
+          <Route exact path="/search" element={<Search />} />
+        </Routes>
+      </ChakraProvider>
     </div>
   );
 }

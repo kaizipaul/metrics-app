6b04c80c37554e736b17d40d08f0ad6d1657858a
Paul Kaizirege <kaizipaul@gmail.com>
2024-03-01 16:22:02 +0300

    update Redux

--- src/Redux/homePage/homePage.js
+++ src/Redux/homePage/homePage.js
@@ -26,14 +26,37 @@ export const getCharacter = createAsyncThunk('home/getCharacter',
     return characterData;
   });
 
+// TODO 1/03/24
+// 1. First go cut your hair
+// 2. Make this slice work
+// 3. export the actions to CharacterCard
+// 4. in the function add an if statement to check if the id matches the action
+
 export const characterSlice = createSlice({
-  name: 'city',
+  name: 'character',
   initialState,
-  reducers: {},
+  reducers: {
+    // Action 1: Add to collection
+    addToCollection: (state, action) => state.map((char) => {
+      if (char.id !== action.payload) return char;
+      return { ...char, added: true };
+    }),
+
+    // Action 2: Remove from collection
+    removeFromCollection: (state, action) => state.map((char) => {
+      if (char.id !== action.payload) return char;
+      return { ...char, added: false };
+    }),
+  },
   extraReducers: (builder) => {
+    // in contrast to the normal redux library, redux toolkit offers a utility function
+    // the utility function is called builder.addCase
+    // it helps to add an action without having to put up a switch statement
     builder.addCase(getCharacter.pending, () => initialState);
     builder.addCase(getCharacter.fulfilled, (state, action) => action.payload);
   },
 });
+
+export const { addToCollection, removeFromCollection } = characterSlice.actions;
 
 export default characterSlice.reducer;

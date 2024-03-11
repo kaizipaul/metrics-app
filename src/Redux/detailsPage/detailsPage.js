8a72a5fe2cea2b164ff55cc5e6f5c3e83f2f52ea
Paul Kaizirege <kaizipaul@gmail.com>
2024-03-11 18:32:29 +0300

    BUG: state does not update when API function is called

--- src/Redux/detailsPage/detailsPage.js
+++ src/Redux/detailsPage/detailsPage.js
@@ -2,21 +2,52 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const apiUrl = 'https://rickandmortyapi.com/api/character/';
 
-export const initialState = true;
-
-export const getDetails = createAsyncThunk('details/getDetails', async (id) => {
-  const response = await fetch(`${apiUrl}/${id}`);
-  const data = await response.json();
-  return data;
+export const getDetails = createAsyncThunk('details/getDetails', async (id, thunkAPI) => {
+  try {
+    const response = await fetch(`${apiUrl}/${id}`);
+    const data = await response.json();
+    if (data.id) {
+      const details = {
+        id: data.id,
+        name: data.name,
+        image: data.image,
+        location_name: data.location.name,
+        location_url: data.location.url,
+      };
+      return details;
+    }
+    return {};
+  } catch (error) {
+    throw thunkAPI.rejectWithValue(error.response.data);
+  }
 });
+
+const initialState = {
+  content: null,
+  status: 'idle',
+  error: null,
+};
 
 export const detailsSlice = createSlice({
   name: 'details',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getDetails.pending, () => initialState);
-    builder.addCase(getDetails.fulfilled, (state, action) => action.payload);
+    builder
+      .addCase(getDetails.pending, (state) => ({
+        ...state,
+        status: 'loading',
+      }))
+      .addCase(getDetails.fulfilled, (state, action) => ({
+        ...state,
+        content: action.payload,
+        status: 'succeeded',
+      }))
+      .addCase(getDetails.rejected, (state, action) => ({
+        ...state,
+        status: 'failed',
+        error: action.error.message,
+      }));
   },
 });
 
